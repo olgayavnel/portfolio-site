bf1929a67ff56ed8b7afad7efb93eb31e135b3b4
Olga Ivanova <olgaivanova@olgas-mbp.speedport.ip>
2022-10-24 17:35:27 +0200

    feat(frontend): removed boilerplate icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const Footer = () => {
   return (
@@ -15,17 +12,6 @@ export const Footer = () => {
             <img src={logo} alt='Logo' />
           </Col>
           <Col size={12} sm={6} className='text-center text-sm-end'>
-            <div className='social-icon'>
-              <a href='#'>
-                <img src={navIcon1} alt='Icon' />
-              </a>
-              <a href='#'>
-                <img src={navIcon2} alt='Icon' />
-              </a>
-              <a href='#'>
-                <img src={navIcon3} alt='Icon' />
-              </a>
-            </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
