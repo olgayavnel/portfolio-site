b94f429a52a9e2d235b7cd52657caa3e81084665
Olga Ivanova <olgaivanova@olgas-mbp.speedport.ip>
2022-11-05 12:06:04 +0100

    feat: adjusted texts

--- src/components/About.js
+++ src/components/About.js
@@ -13,21 +13,21 @@ export const About = () => {
             <h2>About Me</h2>
             <p>
               I like writing code and exploring the world of programming. I'm a
-              self-taught developer with a background in digital marketing. now.
+              self-taught developer with a background in digital marketing.
             </p>
             <p>
-              My experience working as a software dev is about three years I
-              started coding in 2017. Following up my coding journey, I taught
-              myself the fundamentals of computer science and the curriculum
-              offered by the CS degree. After that, I started working with
-              JavaScript and React, and nowadays I'm doing full-stack
-              development, working with React and Django.
+              My experience working as a software dev is about three years now.
+              I started coding in 2017 by teaching myself the fundamentals of
+              computer science and CS degree curriculum. After that, I started
+              working with TypeScript and React, and nowadays I'm working as a
+              full-stack dev, primarily with React, Node, Python, Django,
+              Postgres, and MongoDB.
             </p>
             <p>
               Fun fact: I have a master's degree in political and social science
-              and used to work in a big tech company doing conversion rate
-              optimization. During my time there, I started learning CS and
-              programming on the side and fell in love with it.
+              and used to work in a big tech company optimizing conversion rate.
+              During my time there, I started coding on the side and fell in
+              love with it.
             </p>
             <p>
               P.S. Yavnel is my non-official surname inherited from my grand
